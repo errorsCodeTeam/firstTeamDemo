1232924ac67e67ff33b7034e7235042cf41b7fee
lhc <l1214095974@yeah.net>
2018-10-25 17:47:37 +0800

    ArithmeticTest8

---
+++ ArithmeticTest8.java
@@ -0,0 +1,12 @@
+package visibleArithmeticExercise;
+
+import java.io.IOException;
+
+public class ArithmeticTest8 {
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        MyExGUI exercise = new MyExGUI();
+    }
+
+}
+
